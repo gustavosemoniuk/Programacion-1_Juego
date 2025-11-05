ff889d9d1627054fb98629f99ea5f9c188e08c21
gustavosemoniuk <gustavo.semoniuk@gmail.com>
2025-11-05 20:14:31 -0300

    menu

---
+++ Main/main.py
@@ -0,0 +1,19 @@
+while True:
+    print("\n Generala")
+    print("1. Jugar ")
+    print("2. Estadísticas")
+    print("3. Créditos")
+    print("4. Salir")
+
+    opcion = input("Seleccione una opcion: ")
+
+    if opcion == 1:
+        pass
+    elif opcion == 2:
+        pass    
+    elif opcion == 3:
+        pass
+    elif opcion == 4:
+        break
+    else:
+        print("Opcion incorrecta, Intente nuevamente.")
