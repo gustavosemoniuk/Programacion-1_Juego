d7833505e16b7d7bb7701d9226517c358fe2b00a
Aderian Santiago <santionlyforschool@gmail.com>
2025-11-05 21:33:11 -0300

    a

--- Main/main.py
+++ Main/main.py
@@ -1,19 +1,25 @@
 while True:
-    print("\n Generala")
-    print("1. Jugar ")
+    print("\n" + "=" * 40)
+    print("           🎲 GENERALA  🎲")
+    print("=" * 40)
+    print("1. Jugar")
     print("2. Estadísticas")
     print("3. Créditos")
     print("4. Salir")
+    print("=" * 40)
 
-    opcion = input("Seleccione una opcion: ")
+    opcion = input("Seleccione una opción (1-4): ")
 
-    if opcion == 1:
+    if opcion == "1":
+        print("\nIniciando el juego...\n")
+        # Acá iría la función que maneja la partida
+    elif opcion == "2":
+        print("\nMostrando estadísticas...\n")
+        # Acá iría la función que muestra las estadísticas
+    elif opcion == "3":
         pass
-    elif opcion == 2:
-        pass    
-    elif opcion == 3:
-        pass
-    elif opcion == 4:
+    elif opcion == "4":
+        print("\nGracias por jugar. ¡Hasta la próxima!\n")
         break
     else:
-        print("Opcion incorrecta, Intente nuevamente.")
+        print("\nOpción incorrecta. Intente nuevamente.\n")
